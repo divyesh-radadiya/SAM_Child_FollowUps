dccbb146aa34a6213d6b2fc19242cff50a455ca2
abhijain614 <abhijain614@gmail.com>
2022-03-30 10:49:51 +0530

    Implemented Growth status for a child

--- src/main/java/com/example/samprojectdb/controller/ChildController.java
+++ src/main/java/com/example/samprojectdb/controller/ChildController.java
@@ -1,13 +1,17 @@
 package com.example.samprojectdb.controller;
 
 import com.example.samprojectdb.entity.Child;
+import com.example.samprojectdb.entity.GrowthStatusRules;
 import com.example.samprojectdb.repository.ChildRepo;
+import com.example.samprojectdb.repository.GrowthStatusRulesRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/child")
@@ -15,15 +19,55 @@ public class ChildController {
     @Autowired
     private ChildRepo childRepo;
 
-//    @RequestMapping("/demo")
+    @Autowired
+    private GrowthStatusRulesRepo growthStatusRulesRepo;
+
+    //    @RequestMapping("/demo")
 //    public void display()
 //    {
 //        System.out.println(childRepo.findAll().toString());
 //    }
     @RequestMapping("/findAll")
-    public List<Child> findAll()
-    {
+    public List<Child> findAll() {
         return childRepo.findAll();
+    }
+
+    public Optional<GrowthStatusRules> getGrowthStatus(double ht) {
+        return growthStatusRulesRepo.findById(ht);
+    }
+
+    @GetMapping("/getGrowthStatus/{samId}")
+    public String findGS(@PathVariable("samId") int samId) {
+        Child tempChild = childRepo.findBySamId(samId);
+        if(tempChild==null)
+            return "NA";
+        //System.out.println(childRepo.findHeightBySamId(samId));
+        double ht = tempChild.getHeight();
+        Optional<GrowthStatusRules> growthStatusRules = getGrowthStatus(ht);
+        if (growthStatusRules.isPresent()) {
+            GrowthStatusRules growthStatusRules1 = growthStatusRules.get();
+            double threshold = 0;
+            switch (tempChild.getGender()) {
+                case 'M':
+                    threshold = growthStatusRules1.getBoys3d();
+                    break;
+                case 'F':
+                    threshold = growthStatusRules1.getGirls3d();
+                    break;
+                default:
+                    System.out.println("Some error here in FollowUpController gender switch case!!");
+                    System.exit(0);
+                    break;
+            }
+            if (tempChild.getWeight() < threshold) {
+                System.out.println("SAM");
+                return "SAM";
+            } else {
+                System.out.println("Normal");
+                return "Normal";
+            }
+        }
+        return "NA";
     }
 
     @RequestMapping("/findByName/{name}")
@@ -31,5 +75,4 @@ public class ChildController {
     {
         return childRepo.findChildByName(name);
     }
-
 }
--- src/main/java/com/example/samprojectdb/repository/ChildRepo.java
+++ src/main/java/com/example/samprojectdb/repository/ChildRepo.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ChildRepo extends JpaRepository<Child,Integer> {
 
     Child findChildByName(String name);
+
+    Child findBySamId(Integer samId);
 }
